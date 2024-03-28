d1f11c63d92128af2e8d9ccbe818a9d974bf750e
Tobi Lutke <tobi@shopify.com>
2024-03-28 10:17:30 -0400

    keys

--- src/secrets.sh
+++ src/secrets.sh
@@ -1,66 +1,71 @@
 # Set variables at the script's top level
-id="--identity $HOME/.ssh/id_ed25519"
+id="$HOME/.ssh/id_ed25519"
 secrets_file="${DOTFILES_PATH}/src/secrets.age"
+hostkey_file="${HOME}/.ssh/hostkey"
 secrets_hosts="${DOTFILES_PATH}/src/secrets.hosts"
 
-eage() {
-  echo $secrets_file
-  SECRETS=$(age -d "$id" "$secrets_file" 2>/dev/null) || {
+secrets_edit() {
+  tmpfile=$(mktemp -tsecret)
+
+  age --decrypt -i $id -o $tmpfile $secrets_file || {
     printf "!!! cannot decrypt\n"
+
+    if ! grep -qF -- "$hostkey".pub "$secrets_hosts"; then
+      printf "    add:\n\n%s\n\n  at https://github.com/tobi/dotfiles/edit/main/src/secrets.hosts\n" $(cat $hostkey.pub)
+    fi
+
     return 1
   }
 
-  tmpfile=$(mktemp)
+  crc=$(crc32 $tmpfile)
 
-  printf "%s" "$SECRETS" >"$tmpfile"
-  unset $SECRETS
   nano "$tmpfile" || {
     printf "!!! aborting\n"
     rm "$tmpfile"
     return 1
   }
 
-  age -e -R "$secrets_hosts" "$id" -o "$secrets_file" "$tmpfile" || {
+  if [[ $crc == $(crc32 $tmpfile) ]]; then 
+    printf "!!! no changes in file\n"
+    rm "$tmpfile"
+    return 1
+  fi
+
+  if ! head -n 1 "$tmpfile" | grep -q "^export " > /dev/null; then
+    printf "!!! not valid\n"
+    rm "$tmpfile"
+    return 1
+  fi
+
+  age --encrypt -R $secrets_hosts -i $id -o $secrets_file $tmpfile || {
     printf "!!! cannot encrypt\n"
     rm "$tmpfile"
     return 1
   }
 
   rm "$tmpfile"
+  printf "OK\n"
+
 }
 
-ekey() {
-  if [ ! -f "$HOME/.ssh/hostkey.pub" ]; then
+secrets_generate_hostkey() {
+  if [ ! -f $hostkey ]; then
     printf "generating hostkey...\n"
-    ssh-keygen -t ed25519 -f "$HOME/.ssh/hostkey"
+    ssh-keygen -t ed25519 -f "$hostkey"
   else
-    printf "hostkey already exists...\n"
-  fi
-
-  hostkey_pub=$(<"$HOME/.ssh/hostkey.pub")
-
-  if ! grep -qF -- "$hostkey_pub" "$secrets_hosts"; then
-    printf "add:\n\n%s\n\n  at https://github.com/tobi/dotfiles/edit/main/src/secrets.hosts\n" "$hostkey_pub"
-  else
-    printf "hostkey already in secrets.hosts\n"
+    #printf "hostkey already exists...\n"
   fi
 }
 
-load_secrets() {
-  printf "* loading secrets... "
+secrets() {
+  secrets_generate_hostkey
+  
+  # printf "* loading secrets... "
   if ! command -v age >/dev/null 2>&1; then
-    printf "[FAIL]: age not installed\n"
+    #printf "[FAIL]: age not installed\n"
     return 1
   fi
-  if [ -f "$HOME/.ssh/hostkey" ]; then
-    env=$(age -d -i "$HOME/.ssh/hostkey" "$secrets_file" 2>/dev/null) && {
-      eval "$env"
-      printf "[OK]\n"
-    } || {
-      printf "[FAIL]\n"
-      return 1
-    }
-  else
-    printf "[FAIL]: no ~/.ssh/hostkey\n"
-  fi
+
+  eval $(age --decrypt -i "$HOME/.ssh/hostkey" "$secrets_file" 2>/dev/null)
+
 }
