e88a8464e3954501ffbf1457783da78486ccccd3
Tobi Lutke <tobi@shopify.com>
2024-05-21 09:18:59 -0400

    run stuff reliably in bg via bx

--- src/updates.sh
+++ src/updates.sh
@@ -1,12 +1,12 @@
 local sentinel=/tmp/dotfiles.sentinel
 # check if the sentinel file exists, or if its older than 2 days
 if [ ! -f $sentinel ] || [ $(find $sentinel -mtime +2) ]; then
-  cd $DOTFILES_PATH
-  git pull
-  if [[ $? -eq 0 ]]; then 
-     touch $sentinel
-  else
-     git status
-  fi
-  cd $HOME
+   cd $DOTFILES_PATH
+   git pull
+   if [[ $? -eq 0 ]]; then
+      touch $sentinel
+   else
+      git status
+   fi
+   cd $HOME
 fi
