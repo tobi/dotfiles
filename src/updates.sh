452fc05de55be3ae40d70018dc0283fd79cf104a
Tobi Lutke <tobi@shopify.com>
2024-08-09 10:34:11 -0400

    don't muck with $PWD in dotfiles yo

--- src/updates.sh
+++ src/updates.sh
@@ -1,14 +1,15 @@
+#!/bin/bash
+
 sentinel=/tmp/dotfiles.sentinel
 # check if the sentinel file exists, or if its older than 2 days
 if [ ! -f $sentinel ] || [ $(find $sentinel -mtime +2) ]; then
    cd $DOTFILES_PATH
-   git pull
+   git pull --rebase
    if [[ $? -eq 0 ]]; then
       touch $sentinel
    else
       git status
    fi
-   cd $HOME
 fi
 
 unset sentinel
