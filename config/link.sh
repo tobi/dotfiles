8be20e68f4b7ea7946bfd3a07b70b9b59ad870fa
Tobi Lutke <tobi@shopify.com>
2024-08-19 16:11:20 -0400

    include configs

--- config/link.sh
+++ config/link.sh
@@ -3,14 +3,26 @@ cd $HOME/dotfiles/config
 
 # Loop through all files in the directory
 for file in *; do
+  basename=$(basename "$file")
   # Skip if it's not a file
   [ -f "$file" ] || continue
+  [ "$basename" == "link.sh" ] && continue
+
+  ext=$(echo "$file" | awk -F. '{print $NF}')
+  basename_without_ext=$(echo "$basename" | sed 's/\.'$ext'//g')
 
   # Replace . with / and _ with .
-  destination=$(echo "$file" | sed 's/\./_/g' | sed 's/_/./g')
+  destination=$HOME/$(echo "$basename_without_ext" | sed 's/\./\//g' | sed 's/_/./g').$ext
+
+  if [ -f "$destination" ]; then
+    echo "File $destination already exists"
+    continue
+  fi
+
+  echo "linking $destination to ./$file"
 
   # link the file
-  ln -s "$file" "$destination"
+  ln -s "$HOME/$file" "$destination"
 
   echo "linked $destination to $file"
 done
