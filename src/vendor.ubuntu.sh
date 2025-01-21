4cfd8981adbac184a8c7f3ff04b700230112c527
Tobi Lutke <tobi@shopify.com>
2025-01-21 13:04:30 -0500

    share history

--- src/vendor.ubuntu.sh
+++ src/vendor.ubuntu.sh
@@ -3,3 +3,14 @@ if [[ $tailscale_ip != "" ]]; then
   export IP=$tailscale_ip
 fi
 unset tailscale_ip
+
+isd() {
+  if ! command -v "uvx" >/dev/null 2>&1; then
+    echo "uvx is not installed"
+    echo "Run:"
+    echo "  pipx install uv"
+    return 1
+  fi
+
+  uvx --python=3.12 --from git+https://github.com/isd-project/isd isd
+}
